128c93f2baf3175af3de16d0e4b62b04c95e7dc6
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-06-05 08:54:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment_5._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence.");
+            string inputSentence = Console.ReadLine();
+            int lengthOfLastWord = LengthOfLastWord(inputSentence);
+            Console.WriteLine($"The last word is {lengthOfLastWord} characters long.");
+        }
+        static int LengthOfLastWord( string s)
+        {
+            s = s.Trim();
+            string[] words = s.Split();
+            string lastWord = words.Length > 0 ? words[words.Length - 1] : "";
+            return lastWord.Length;
+        }
+
+    }
+}
